2e4812323f9c0295cb2768edf57079880cdb9cfe
00qnavry00 <67124050+00qnavry00@users.noreply.github.com>
2023-10-09 03:24:33 -0700

    Making Character adding connected through backend

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -5,7 +5,19 @@ import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 
 function MyApp() {
-	const [characters, setCharacters] = useState([]); 
+	const [characters, setCharacters] = useState([]);
+        async function makePostCall(person){
+          try {
+             const response = await axios.post('http://localhost:8000/users', person);
+             return response;
+          }
+          catch (error) {
+             console.log(error);
+             return false;
+          }
+        }
+
+ 
         useEffect(() => {
           fetchAll().then( result => {
              if (result)
@@ -23,9 +35,13 @@ function MyApp() {
              return false;         
           }
         }
-        function updateList(person) {
-          setCharacters([...characters, person]);
+        function updateList(person) { 
+          makePostCall(person).then( result => {
+          if (result && result.status === 200)
+             setCharacters([...characters, person] );
+          });
         }
+
 	function removeOneCharacter (index) {
 	    const updated = characters.filter((character, i) => {
 	        return i !== index
